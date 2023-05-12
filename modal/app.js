ca53d0167ac53dfa0389a4101c7f129b9bb4bc37
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-12 11:41:43 -0400

    add modal html css and js

--- modal/app.js
+++ modal/app.js
@@ -1,3 +1,9 @@
+const getElement = (selector) => {
+  const el = document.querySelector(selector);
+  if (el) return el;
+  throw new Error(`Please double check the ${selector}`);
+};
+
 const modalBtn = getElement(".modal-btn");
 const modal = getElement(".modal-overlay");
 const closeBtn = getElement(".close-btn");
